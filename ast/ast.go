c2193ccff9317409eb6bcd6554566accfcd06975
RATIU5 <jcmemmott20@outlook.com>
2023-07-18 18:39:58 -0600

    add: return statmenet String fn

--- ast/ast.go
+++ ast/ast.go
@@ -98,6 +98,20 @@ func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
 }
 
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
 type ExpressionStatement struct {
 	Token      token.Token
 	Expression Expression

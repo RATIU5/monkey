067406a4c821414fa09584662557a74711c6aece
RATIU5 <jcmemmott20@outlook.com>
2023-10-20 10:58:51 -0600

    add: parser test

---
+++ parser/parser.go
@@ -0,0 +1,43 @@
+package parser
+
+import (
+	"github.com/RATIU5/monkey/ast"
+	"github.com/RATIU5/monkey/lexer"
+	"github.com/RATIU5/monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Ensure both curToken and peekToken are set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
